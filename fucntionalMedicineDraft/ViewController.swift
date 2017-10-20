284b9f605e5337c8ebaaf53565e5be7fb08842eb
zlintz14 <zakl14@live.unc.edu>
2017-10-20 17:39:09 -0400

    final fix
    should fix all my errors, this is most up to date and working version

--- fucntionalMedicineDraft/ViewController.swift
+++ fucntionalMedicineDraft/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Outlet
+    //added random comments to commit
+    @IBOutlet weak var enterTextField: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    //MARK: Action
+    //added random comment so I could commit again after my errors
+    @IBAction func enter(_ sender: Any) {
+        if enterTextField.text != ""{
+            performSegue(withIdentifier: "segueOne", sender: self)
+        }
+    }
+
+    // ! operator means force unwrap
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let secondController = segue.destination as! ViewControllerTwo
+        secondController.myString = enterTextField.text!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
